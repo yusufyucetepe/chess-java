a3fbed63cc6374eca8c60bea43961d0abc37f8f9
yusufyucetepe <yusufyucetepe06@gmail.com>
2026-04-27 17:38:23 +0300

    Setup

---
+++ src/main.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            Game game = new Game();
+            game.setVisible(true);
+        });
+    }
+}
